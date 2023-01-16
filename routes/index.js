22b171787e7916a96e6619f8c4234b5e46dc69c9
Patrick <kelly.patj@gmail.com>
2023-01-16 15:56:25 -0500

    passes all products and components to home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 import { Router } from 'express'
+import { Component } from '../models/component.js'
+import { Product } from '../models/product.js'
 
 const router = Router()
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Home Page' })
+  Product.find({})
+  .then(products =>{
+    Component.find({})
+    .then(components =>{
+      res.render('index', {
+        title: 'Home Page',
+        products,
+        components
+      })
+    })
+  })
 })
 
 export {
