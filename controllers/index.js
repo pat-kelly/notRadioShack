b532bc28c44665bb6048dd3a4a58c640db555f60
Patrick <kelly.patj@gmail.com>
2023-01-17 10:54:04 -0500

    impliments regex powered search

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ import { Product } from '../models/product.js'
 
 function index (req, res) {
   let prodFilter = false, compFilter = false;
-  console.log('query',req.query.prodFilter);
+  // console.log('query',req.query.prodFilter);
   if(req.query.prodFilter){
     switch (req.query.prodFilter) {
       case 'products':
@@ -21,17 +21,49 @@ function index (req, res) {
   .then(products =>{
     Component.find( compFilter ? {} : {filterOut: true} )
     .then(components =>{
-      console.log('products', products);
-      console.log('components', components)
+      // console.log('products', products);
+      // console.log('components', components)
       res.render('index', {
         title: 'Home Page',
         products,
         components
       })
     })
+    .catch(err =>{
+      console.error(err);
+      res.redirect('/');
+    })
+  })
+  .catch(err =>{
+    console.error(err);
+    res.redirect('/');
+  })
+}
+
+function search(req, res){
+  const searchTerms = new RegExp(req.query.searchTerms, "i");
+  Product.find({name: searchTerms})
+  .then(products =>{
+    Component.find({name: searchTerms})
+    .then(components =>{
+      res.render('index', {
+        title: 'Home Page',
+        products,
+        components
+      })
+    })
+    .catch(err =>{
+      console.error(err);
+      res.redirect('/');
+    })
+  })
+  .catch(err =>{
+    console.error(err);
+    res.redirect('/');
   })
 }
 
 export {
   index,
+  search
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import * as indexCtrl from '../controllers/index.js'
+import { index } from '../controllers/products.js';
 
 const router = Router()
 
 //GET /
 router.get('/', indexCtrl.index);
 
+//GET /search/?searchQuery
+router.get('/search', indexCtrl.search);
 
 
 export {
